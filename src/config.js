316ab61d6d2b45750c08fd250c46957b9b162f5a
RushilXD <rushilgoel2008@gmail.com>
2025-10-29 20:17:49 +0530

    version 1.1.0

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-console.log("Version 1.0.1");
-console.log("name: initial-git");
+console.log("Version 1.1.0");
+console.log("name: init-git");
--- version.js
+++ version.js
@@ -1,2 +1,2 @@
 console.log("Current Version:-");
-console.log("Version 1.0.1");
+console.log("Version 1.1.0");
